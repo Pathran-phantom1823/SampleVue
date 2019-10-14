36fc2b53ceb2e756b50cc4aa538f4776a09ca0e8
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-10-14 15:48:47 +0800

    initial commit

---
+++ logi-form/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+<div id="app" class="small-container">
+    <h1>Employees</h1>
+    <employee-form @add:employee="addEmployee"/>
+    <employee-table v-bind:employees="employees" />
+</div>
+</template>
+
+<script>
+import EmployeeForm from '@/components/EmployeeForm.vue'
+import employeeTable from '@/components/employeeTable.vue'
+
+export default {
+    name: 'app',
+    components: {
+        EmployeeForm,
+        employeeTable
+    },
+    data() {
+        return {
+            employees: [{
+                    id: 1,
+                    name: 'John Patrick',
+                    email: 'john@gmail.com',
+                },
+                {
+                    id: 2,
+                    name: 'Patrick',
+                    email: 'pat@gmail.com'
+                }
+            ]
+        }
+    },
+    methods:{
+      addEmployee(employee){
+        const lastId=
+          this.employees.length>0
+            ? this.employees[this.employees.length-1].id
+            :0;
+        const id = lastId-1;
+        const newEmployee={...employee, id};
+
+        this.employees = [...this.employees, newEmployee]
+      }
+    }
+}
+</script>
+
+<style>
+button {
+    background: #009435;
+    border: 1px solid #009435;
+}
+
+.small-container {
+    max-width: 680px;
+}
+</style>
---
+++ logi-form/src/components/employeeTable.vue
@@ -0,0 +1,35 @@
+<template>
+<div id="employee-table">
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="employee in employees" v-bind:key="employee.id">
+                <td>{{employee.name}}</td>
+                <td>{{employee.email}} </td>
+
+                <td>
+                <button>Edit</button>
+                <button>Delete</button>
+                </td>   
+            </tr>
+        </tbody>
+    </table>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'employee-table',
+    props: {
+        employees: Array,
+    }
+}
+</script>
+
+<style scoped>
+</style>
